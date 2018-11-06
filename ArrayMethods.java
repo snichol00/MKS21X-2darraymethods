e7109e1d48b51475b77785fc5fc9a1570903419e
Sophie Nichol <snichol00@stuy.edu>
2018-11-06 17:09:09 -0500

    fixed uneven array errors

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -4,9 +4,14 @@ public class ArrayMethods{
    */
    public static int rowSum(int[][] ary, int x){
      int sum = 0;
-     for (int i = 0; i < ary[x].length; i++){
-       sum += ary[x][i];
+     try{
+       for (int i = 0; i < ary[x].length; i++){
+         sum += ary[x][i];
+       }
      }
+     catch(IndexOutOfBoundsException e){
+         sum += 0;
+       }
      return sum;
    }
     //returns the sum of the elements in Row x of ary.
@@ -15,20 +20,38 @@ public class ArrayMethods{
    public static int columnSum(int[][] ary, int x){
      int sum = 0;
      for (int i = 0; i < ary.length; i++){
-       sum += ary[i][x];
+       try{
+         sum += ary[i][x];
+       }
+       catch(IndexOutOfBoundsException e){
+         sum += 0;
+       }
      }
      return sum;
    }
     //returns the sum of the elements in Column x of ary (careful with rows of different lengths!).
     //When a row is not long enough to reach the column count it as a zero. (NO indexOutOfBounds should ever occur)
-
+  public static int maxRowLength(int[][]ary){
+    int max = 0;
+    for (int i = 0; i < ary.length; i++){
+      if (ary[i].length > max){
+        max = ary[i].length;
+      }
+    }
+    return max;
+  }
    /*
    *PART 2 - use prior methods where appropriate
    */
    public static int[] allRowSums(int[][] ary){
      int[] sums = new int[ary.length];
      for (int i = 0; i < ary.length; i++){
-       sums[i] = rowSum(ary, i);
+       try{
+         sums[i] = rowSum(ary, i);
+       }
+       catch(IndexOutOfBoundsException e){
+         sums[i] = 0;
+       }
      }
      return sums;
    }
@@ -36,9 +59,14 @@ public class ArrayMethods{
     //Index i of the return array contains the sum of elements in row i.
 
     public static int[] allColSums(int[][] ary){
-      int[] sums = new int[ary[0].length];
-      for (int i = 0; i < ary[0].length; i++){
-        sums[i] = columnSum(ary, i);
+      int[] sums = new int[maxRowLength(ary)];
+      for (int idx = 0;idx < maxRowLength(ary); idx++){
+        try{
+          sums[idx] = columnSum(ary, idx);
+        }
+        catch(IndexOutOfBoundsException e){
+          sums[idx] = 0;
+        }
       }
       return sums;
     }
@@ -53,18 +81,19 @@ public class ArrayMethods{
    */
    public static boolean isRowMagic(int[][] ary){
      int sum = rowSum(ary, 0);
-     for (int i = 0; i < ary.length; i++){
-       if (rowSum(ary, i) != sum){
+     for (int idx = 0; idx < ary.length; idx++){
+       if (rowSum(ary, idx) != sum){
          return false;
        }
      }
+
      return true;
    }
      //checks if the array is row-magic (this means that every row has the same row sum).
 
    public static boolean isColumnMagic(int[][] ary){
      int sum = columnSum(ary, 0);
-     for (int i = 0; i < ary[0].length; i++){
+     for (int i = 0; i < maxRowLength(ary); i++){
        if (columnSum(ary, i) != sum){
          return false;
        }
